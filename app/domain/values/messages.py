4d638d273efb304b1cc8d77e086031da8392fc4d
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2024-03-22 23:15:37 +0800

    added text and entities refactoring

--- app/domain/values/messages.py
+++ app/domain/values/messages.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from domain.exceptions.messages import TextToolLongExceprion
+from domain.exceptions.messages import EmptyTextException, TitleTooLongException
 from domain.values.base import BaseValueObject
 
 
@@ -9,8 +9,20 @@ class Text(BaseValueObject):
     value: str
 
     def validate(self):
+        if not self.value:
+            raise EmptyTextException()
+
+    def as_generic_type(self):
+        return str(self.value)
+
+
+@dataclass(frozen=True)
+class Title(BaseValueObject):
+    def validate(self):
+        if not self.value:
+            raise EmptyTextException()
         if len(self.value) > 255:
-            raise TextToolLongExceprion(self.value)
+            raise TitleTooLongException(self.value)
 
     def as_generic_type(self):
         return str(self.value)
