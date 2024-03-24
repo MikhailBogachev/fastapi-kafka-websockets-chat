f530adb1578e458626813a9f297b30a2594935a2
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2024-03-24 17:33:06 +0800

    added repos and mediator handler fix

--- app/logic/mediator.py
+++ app/logic/mediator.py
@@ -20,10 +20,10 @@ class Mediator:
     )
 
     def register_event(self, event: ET, event_handlers: Iterable[EventHandler[ET, ER]]):
-        self.events_map[event.__class__].extend(event_handlers)
+        self.events_map[event].extend(event_handlers)
 
     def register_command(self, command: CT, command_handlers: Iterable[EventHandler[CT, CR]]):
-        self.commands_map[command.__class__].extend(command_handlers)
+        self.commands_map[command].extend(command_handlers)
 
     async def handle_event(self, event: BaseEvent) -> Iterable[ER]:
         event_type = event.__class__
--- app/tests/confest.py
+++ app/tests/confest.py
@@ -1,18 +1,17 @@
 from pytest import fixture
-
 from infra.repositories.messages import BaseChatRepository, MemoryChatRepository
 from logic.init import init_mediator
 from logic.mediator import Mediator
 
 
-@fixture(scope='package')
+@fixture(scope='function')
 def chat_repository() -> MemoryChatRepository:
     return MemoryChatRepository()
 
 
-@fixture(scope='package')
-def mediator(chaat_repository: BaseChatRepository) -> Mediator:
+@fixture(scope='function')
+def mediator(chat_repository: BaseChatRepository) -> Mediator:
     mediator = Mediator()
-    init_mediator(mediator=mediator, chat_repository=chaat_repository)
+    init_mediator(mediator=mediator, chat_repository=chat_repository)
 
     return mediator
